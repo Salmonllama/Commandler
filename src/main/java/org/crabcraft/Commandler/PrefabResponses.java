ffa79afc8b58eaed621f1fe5c43dc97901e63005
Crablet <a.gryczewski999@gmail.com>
2018-12-16 06:59:50 -0500

    Added response for improper command usage

--- src/main/java/org/crabcraft/Commandler/PrefabResponses.java
+++ src/main/java/org/crabcraft/Commandler/PrefabResponses.java
@@ -8,16 +8,24 @@ import java.awt.*;
 class PrefabResponses {
 
     static EmbedBuilder noPermissions(MessageCreateEvent event, String permission) {
-
         return new EmbedBuilder()
             .setColor(Color.RED)
             .setAuthor(event.getApi().getYourself())
             .addField("Missing Permissions:", permission);
     }
 
+    static EmbedBuilder improperUsage(MessageCreateEvent event, String usage) {
+        // ?Would the command description would be viable in this embed.
+        return new EmbedBuilder()
+            .setColor(Color.YELLOW)
+            .setAuthor(event.getApi().getYourself())
+            .setTitle("Improper Command Usage")
+            .addField("Usage:", usage);
+    }
+
     /*
      * TODO: Add bundled prefab responses for:
-     *  - Improper command usage, includes proper command usage + description
+     *  - Improper command usage, includes proper command usage + description // DONE
      *  - Errors, or warnings, with custom fields for displaying an error message
      */
 
