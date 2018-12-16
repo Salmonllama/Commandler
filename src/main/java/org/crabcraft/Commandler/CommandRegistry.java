bc67a7b2558b387132e6ef667509b5deec7698f5
Crablet <a.gryczewski999@gmail.com>
2018-12-16 06:03:16 -0500

    Began documentation for Javadocs

--- src/main/java/org/crabcraft/Commandler/CommandRegistry.java
+++ src/main/java/org/crabcraft/Commandler/CommandRegistry.java
@@ -1,11 +1,12 @@
 package org.crabcraft.Commandler;
 
-import org.javacord.api.DiscordApi;
-
 import java.util.TreeMap;
 
+    /*
+    * The base registry.
+    * Used to register commands and track registered commands.
+    */
 public class CommandRegistry {
-    private DiscordApi api;
     private static TreeMap<String, Command> commands;
 
     public CommandRegistry() {
@@ -16,11 +17,23 @@ public class CommandRegistry {
         FrameworkDB.firstTimeSetup();
     }
 
+    /**
+    * Registers the command, adding it to the global registry.
+    * *Returns the command, making it easy to add a messageCreateListener through Javacord and register the command.
+    *
+    * @param command The command to be registered
+    * @return The registered command
+    */
     public Command registerCommand(Command command) {
         commands.put(command.Aliases().get(0), command);
         return command;
     }
 
+    /**
+     * Gets the list of registered commands.
+     * 
+     * @return The TreeMap of commands
+     */
     public TreeMap<String, Command> getCommands() {
         return commands;
     }
