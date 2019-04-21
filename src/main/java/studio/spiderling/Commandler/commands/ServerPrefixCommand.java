9b9ab71819ea37ae10d981313e00147579dcba3f
Aleksei Fox <a.gryczewski999@gmail.com>
2019-04-21 12:51:09 -0400

    Finalised ServerPrefixCommand

--- src/main/java/studio/spiderling/Commandler/commands/ServerPrefixCommand.java
+++ src/main/java/studio/spiderling/Commandler/commands/ServerPrefixCommand.java
@@ -1,37 +1,63 @@
 package studio.spiderling.Commandler.commands;
 
 import studio.spiderling.Commandler.Command;
+import studio.spiderling.Commandler.FrameworkDB;
+import studio.spiderling.Commandler.PrefabResponses;
+
 import java.util.List;
 import java.util.Arrays;
+
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class ServerPrefixCommand extends Command {
     @Override
     public String Name() { return "Server Prefix Command"; }
     @Override
-    public List<String> Aliases() { return Arrays.asList("prefix", "serverprefix", "sp"); }
+    public String Description() { return "View or change the server-specific prefix"; }
     @Override
-    public String Description() { return "View or change the server prefix"; }
+    public String Category() { return "Commandler"; }
     @Override
-    public String Usage() { return "example <arguments>"; }
+    public String Usage() { return "prefix [String newPrefix]"; }
     @Override
-    public List<String> Permissions() { return Arrays.asList("none"); }
+    public List<String> Aliases() { return Arrays.asList("prefix", "serverprefix", "sp", "setprefix", "changeprefix"); }
     @Override
-    public String Category() { return "Example Commands"; }
+    public List<String> Permissions() { return Arrays.asList("MANAGE_SERVER"); }
 
     @Override
     public void onCommand(MessageCreateEvent event, String[] args) {
-        String response;
-
-        if (args.length > 0) {
-            response = String.join(" ", args);
-
-            sendResponse(response);
+        if (event.isPrivateMessage()) {
+            sendResponse("This command is for managing the *server* prefix. Please use this again in a server.");
             return;
         }
 
-        response = "Welcome to the Commandler framework!";
-        sendResponse(response);
-        return;
+        if (args.length == 0) {
+            EmbedBuilder embed = new EmbedBuilder();
+
+            event.getServer().ifPresent(server -> {
+                embed.setTitle("Current Prefix")
+                    .setDescription("```\n" + FrameworkDB.getServerPrefix(server.getIdAsString()) + "\n```");
+            });
+
+            sendResponse(embed);
+            return;
+        }
+        else if (args.length == 1) {
+            EmbedBuilder embed = new EmbedBuilder();
+
+            event.getServer().ifPresent(server -> {
+                FrameworkDB.setServerPrefix(server.getIdAsString(), args[1]);
+
+                embed.setTitle("Prefix Changed")
+                    .setDescription("**New Prefix:\n" + "```\n" + args[1] + "\n```");
+            });
+
+            sendResponse(embed);
+            return;
+        }
+        else {
+            sendResponse(PrefabResponses.improperUsage(event, this.Usage()));
+        }
     }
 }
